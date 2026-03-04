b9fba94114b5dfeb97b8225407de2728dc1a352c
KimJ6 <hwanma0578@gmail.com>
2026-03-05 00:24:17 +0900

    Update app.js

--- app.js
+++ app.js
@@ -416,8 +416,8 @@ window.draw = function () {
   textAlign(LEFT, BOTTOM);
   text(val2, rightValueX, height - 15);
 
-  let leftColonX = 65;  
-  let leftValueX = 1350; 
+  let leftColonX = 55;  
+  let leftValueX = 120; 
 
   fill(100);
   textSize(11);
